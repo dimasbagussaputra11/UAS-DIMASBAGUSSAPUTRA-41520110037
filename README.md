118508e0d8e157e9f476480fcaec4c73f51a5e87
dimasbagussaputra11 <dimasbagussaputra37@gmail.com>
2024-01-13 10:06:26 +0700

    first

---
+++ README.md
@@ -0,0 +1,53 @@
+Final task for Mobile Programing Subject
+
+Author by : Dimas Bagus Saputra
+
+NIM : 41520110037
+
+Mercu Buana University
+
+Regular 2
+
+Menteng Campus
+
+Aplikasi pemesan makanan adalah aplikasi yang memungkinkan pengguna untuk memesan makanan dari restoran tertentu secara online. Aplikasi ini biasanya tersedia untuk perangkat seluler, seperti smartphone dan tablet.
+
+<img width="170" alt="Screenshot 2024-01-13 020022" src="https://github.com/dimasbagussaputra11/Project-Food-Order/assets/61934772/fd443db6-863e-48ea-ac95-0212498520bf">
+<img width="173" alt="Screenshot 2024-01-13 020412" src="https://github.com/dimasbagussaputra11/Project-Food-Order/assets/61934772/ab47cf1e-0029-4514-850d-82035ee7b71b">
+<img width="173" alt="Screenshot 2024-01-13 020544" src="https://github.com/dimasbagussaputra11/Project-Food-Order/assets/61934772/30b857b9-256d-4669-8964-1ef032546c86">
+
+Aplikasi ini bernama Aplikasi TOKO MADURA. Aplikasi ini merupakan aplikasi simulasi untuk pemesanan makanan seperti pecel lele, snack, dan lain-lain.
+
+Pemesanan Makanan:
+
+Fitur pemesanan makanan memungkinkan pengguna untuk dengan mudah dan efisien menempatkan pesanan mereka. Beberapa aspek utama dari fitur ini mencakup:
+
+Penambahan ke Keranjang:
+- Pengguna dapat menambahkan item-menu yang mereka pilih ke dalam keranjang belanja.
+- Mereka dapat mengelola jumlah pesanan dan melihat ringkasan pesanan sebelum melanjutkan ke proses pembayaran.
+
+Proses Check Out:
+- Setelah memilih makanan, pengguna dapat melanjutkan ke proses check-out untuk menyelesaikan pemesanan mereka.
+  
+Pembayaran:
+- Fitur pembayaran merupakan langkah kunci dalam aplikasi pemesanan makanan. Beberapa elemen utama dari fitur ini melibatkan:
+
+Konfirmasi Harga:
+- Pengguna diberikan ringkasan total pesanan, termasuk harga makanan, biaya pengiriman (jika ada), dan pajak sebelum melanjutkan ke proses pembayaran.
+
+Konfirmasi Pesanan:
+- Fitur konfirmasi pesanan memberikan keyakinan kepada pengguna bahwa pesanan mereka telah diterima dan sedang diproses. Beberapa aspek utama dari fitur ini mencakup:
+
+Ringkasan Pesanan:
+- Pengguna dapat melihat ringkasan pesanan mereka, termasuk item-menu yang mereka pilih, jumlah, dan total harga.
+
+Untuk mencoba aplikasi ini silahkan ikuti langkah2 berikut ini:
+- Download Zip dari project ini pada menu Code
+- Simpan File Zip yang akan di download
+- Setelah proses download selesai, silahkan extract seluruh file yang ada didalam file Zip tersebut
+- Setelah proses unzip selesai, Buka aplikasi android studio
+- pada Layar tampilan awal aplikasi android studio (Welcome Page), Silahkan Klik menu titik bintang di pojok kanan Pilih Menu Import Project
+- Arahkan ke Project Folder tempat kita mengekstrak file zip tadi,
+- Masuk dan arahkan kedalam subfolder yang di ekstract
+- Terdapat folder project ( pilih folder Project-Food-Order )
+- Kemudian klik OK
---
+++ app/src/main/java/com/example/project/Activity/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.project.Adapter.CategoryAdapter;
+import com.example.project.Adapter.PopularAdapter;
+import com.example.project.Domain.CategoryDomain;
+import com.example.project.Domain.FoodDomain;
+import com.example.project.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+        recyclerViewPopular();
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.card_btn);
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CartListActivity.class));
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void recyclerViewPopular() {
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewPopularList = findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodlist = new ArrayList<>();
+        foodlist.add(new FoodDomain("Pecel Lele", "lele", "Pecel Lele yang sangat delicious, Khas Indonesia", 5.50));
+        foodlist.add(new FoodDomain("Es Teh", "teh", "Es Teh dengan gula atau tawar terasa nikmat dan hilang dahaga", 1.5));
+        foodlist.add(new FoodDomain("Kebab", "kebab", " Kebab Khas Turkey dengan daging sapi atau kambing", 2.9));
+
+        adapter2 = new PopularAdapter(foodlist);
+        recyclerViewPopularList.setAdapter(adapter2);
+
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> categoryList = new ArrayList<>();
+        categoryList.add(new CategoryDomain("Pecel lele", "pecellele"));
+        categoryList.add(new CategoryDomain("Kebab", "kebab"));
+        categoryList.add(new CategoryDomain("Dessert", "dessert"));
+        categoryList.add(new CategoryDomain("Minuman", "cat_6"));
+        categoryList.add(new CategoryDomain("Cemilan", "cemilan"));
+
+        adapter = new CategoryAdapter(categoryList);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+}
